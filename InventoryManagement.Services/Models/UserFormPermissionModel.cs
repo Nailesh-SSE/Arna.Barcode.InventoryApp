497c737b0c0a2676fa1a3f380523f22da03ee70c
pratham sadhu <Sadhupratham100@gmail.com>
2025-12-29 18:11:39 +0530

    add filter for factoryAdmin

--- InventoryManagement.Services/Models/UserFormPermissionModel.cs
+++ InventoryManagement.Services/Models/UserFormPermissionModel.cs
@@ -3,7 +3,7 @@
 public class UserFormPermissionModel
 {
     public string Route { get; set; } = string.Empty;
-
+    public int FormId { get; set; } = 0;
     public bool CanView { get; set; }
     public bool CanCreate { get; set; }
     public bool CanEdit { get; set; }
