72561cfe021d60325946bee63bc99ec98380765d
Sadhupranay <102317179+Sadhupranay@users.noreply.github.com>
2026-04-13 16:29:28 +0530

    added export button in new barcode report

--- InventoryManagement.Services/Reports/Interfaces/IReturnedBarcodeMappingReportService.cs
+++ InventoryManagement.Services/Reports/Interfaces/IReturnedBarcodeMappingReportService.cs
@@ -5,5 +5,6 @@ namespace InventoryManagement.Services.Reports.Interfaces;
 public interface IReturnedBarcodeMappingReportService
 {
     Task<ReturnBarcodeMappingReportResult> GenerateReturnBarcodeMappingReportAsync(SaleReturnFilter filter);
+    Task<byte[]> ExportToExcelAsync(SaleReturnFilter filter);
 
 }
